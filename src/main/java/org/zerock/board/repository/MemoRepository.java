e3528ff4065b12ccaafdfed85b7d41eb44cfaec3
10-5\kkw <ktndud2@icloud.com>
2024-04-09 11:35:01 +0900

    2024-04-09 쿼리메서드 네이티브쿼리

--- src/main/java/org/zerock/board/repository/MemoRepository.java
+++ src/main/java/org/zerock/board/repository/MemoRepository.java
@@ -1,7 +1,14 @@
 package org.zerock.board.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.board.entity.Memo;
+
+import java.util.List;
+import java.util.Objects;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
     // extends JpaRepository<엔티티명, pk타입>
@@ -13,6 +20,46 @@ public interface MemoRepository extends JpaRepository<Memo, Long> {
     // select 작업 : findById(키 타입), getOne(키 타입)
     // update 작업 : save(엔티티 객체)
     // delete 작업 : deleteById(키 타입),  delete(엔티티 객체)
+
+    //쿼리메서드는 메서드명 자체가 쿼리문으로 동작
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
+    //List<Memo> 리턴타입 -> 리스트타입에 객체는 memo
+    //매개값으로 받은 from ~ to 까지 select 진행하여 리스트로 리턴하는 쿼리메서드
+
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
+    //Page<Memo> 리턴타입 -> 페이징 타입에 객체는 memo
+    //매개값으로 받은 from ~ to 까지 select 진행하여 페이징 타입으로 리턴하는 쿼리메서드
+
+    //예를 들어서 10보다 작은 데이터를 삭제한다.
+    void deleteMemoByMnoLessThan(Long num);
+
+    //@Qeury 는 순수한 sql 쿼리문으로 작성한다. 단, 테이블 명이 아니라 엔티티 명으로 사용
+    @Query("SELECT m FROM Memo m ORDER BY m.mno DESC")
+    List<Memo> getListDesc(); //내가 만든 메서드명!
+
+    //매개값이 있는 @Query문 :값 (타입으로 받음)
+    @Query("UPDATE Memo m SET m.memoText = :memoText WHERE m.mno = :mno")
+    int updateMemoText(@Param("mno") Long mno, @Param("memoText") String memoText);
+
+    //매개값이 있는 @Query문 :값 (객체로 받음)
+    @Query("UPDATE Memo m SET m.memoText = : #{memoBean.memoText} WHERE m.mno = : #{memoBean.mno}")
+    int updateMemoText(@Param("memoBean") Memo memo);
+
+    //@Query 메서드로 페이징 처리해보기 -> 리턴 타입이 Page<Memo> => 참고로 이건 많이 안씀~!
+    @Query(value = "SELECT m FROM Memo m WHERE m.mno > :mno",
+            countQuery = "SELECT count(m) FROM Memo m WHERE m.mno > :mno")
+    Page<Memo> getListWithQuery(Long mno, Pageable pageable);
+
+    //db에 존재하지 않는 값 처리해보기 : 예를 들어서 날짜
+    @Query(value = "SELECT m.mno, m.memoText, current_date FROM Memo m WHERE m.mno > :mno",
+            countQuery = "SELECT count(m) FROM Memo m WHERE m.mno > :mno")
+    Page<Object[]> getListWithQueryObject(Long mno, Pageable pageable);
+
+    //Native Sql 처리 : db용 쿼리로 사용하는 기법 -> nativeQuery = true (이걸 넣어줌으로서 테이블명 대신 엔티티를 사용하게 됨)
+    //엔티티 대신에 테이블명으로 사용
+    @Query(value = "SELECT * FROM memo WHERE mno > 0", nativeQuery = true)
+    List<Object[]> getNativeResult();
+
 
     //JPA에서 crud 담당
 
--- src/test/java/org/zerock/board/repository/MemoRepositoryTests.java
+++ src/test/java/org/zerock/board/repository/MemoRepositoryTests.java
@@ -7,9 +7,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.board.entity.Memo;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -177,6 +179,35 @@ public class MemoRepositoryTests {
         }
 
     }
+
+    @Test
+    public void testQueryMethods() {
+        List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(70L, 80L);
+        //memoRepository에 있는 쿼리메서드를 실행하여 리스트 객체로 받음
+        for (Memo memo:list) {
+            System.out.println(memo);
+        } //받은 리스트 객체를 for문을 이용해 콘솔 출력
+    }
+
+    @Test
+    public void testQueryMethodWithPage() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+        //페이지 타입은 of를 이용해서 요청을 처리함, 0번에 10개를 mno를 기준으로 내림차순정렬을 매개값으로 전달
+
+        Page<Memo> result = memoRepository.findByMnoBetween(10L, 50L, pageable);
+
+        result.get().forEach(memo -> System.out.println(memo));
+
+    }
+
+    @Test
+    @Transactional //delete 에서는 2개의 쿼리문이 동작해야함
+    @Commit //delete인 경우 auto commit이 안되기 때문에 commit을 넣어줘야함
+    public void testDeleteQueryMethod() {
+        //쿼리 메서드로 delete 처리를 하면 9번의 쿼리문이 전달되기 때문에 매우 비효율적! => @Query 를 사용해야 좋다!
+        memoRepository.deleteMemoByMnoLessThan(10L);
+    }
+
 
 
 }
